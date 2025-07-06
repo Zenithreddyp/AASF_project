18d957c3a26a42e3afa78c34d815830eca258ffe
Zenith Reddy <zenithreddy696@gmail.com>
2025-07-06 23:19:17 +0530

    resolved url newaddress

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("shippingaddress/",views.ListuserShippingAddress.as_view(),name="address-List"),
-    path("/new/shippingaddress/",views.CreateShippingAddressView.as_view(),name="Create-address"),
+    path("new/shippingaddress/",views.CreateShippingAddressView.as_view(),name="Create-address"),
    path("shippingaddress/delete/<int:pk>/", views.DeleteShippingAddress.as_view(), name="delete-address")
     
 ]
